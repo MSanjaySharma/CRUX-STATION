c4189273f6c26d1af28324151366bb61ea62b5a0
M Sanjay Sharma <65958268+MSanjaySharma@users.noreply.github.com>
2023-04-05 22:08:12 +0530

    Update next.config.js

--- client/next.config.js
+++ client/next.config.js
@@ -2,9 +2,9 @@ module.exports = {
   publicRuntimeConfig: {
     APP_NAME: "CRUXSTATION",
     APP_API_DEVELOPMENT: "http://localhost:3000",
-    APP_API_PRODUCTION: "https://fe1-cs.herokuapp.com",
+    APP_API_PRODUCTION: "https://crux-station.onrender.com",
     API_DEVELOPMENT: "http://localhost:7331/apiv1",
-    API_PRODUCTION: "https://be1-cs.herokuapp.com/apiv1",
+    API_PRODUCTION: "https://crux-station-server.onrender.com/apiv1/",
     PRODUCTION: true,
   },
 };
